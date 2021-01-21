efd5bc4145977d76eff0f606b85c17d4a4ed4181
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2021-01-21 17:38:46 -0500

    iteration 2

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,36 @@
-// iteration 2
+// iteration 1.1
+
+const mongoose = require("mongoose");
+
+require("../configs/db.config");
+
+const Celebrity = require("../models/celebrity.model");
+
+const celebrities = [
+    { name: "DJ Khaled", 
+        occupation: "record producer",
+        catchPhrase: "Another One",
+        },
+        { name: "Ariana Grande", 
+        occupation: "singer",
+        catchPhrase: "Say It With Emojis",
+        },
+        { name: "Walter Cronkite", 
+        occupation: "broadcaster",
+        catchPhrase: "And That's The Way It Is",
+        }
+    
+  
+];
+
+// script that gets run and actually seeds the database
+
+// node bin/seeds.js
+
+Celebrity.create(celebrities)
+  .then((celebritiesFromDB) => {
+    console.log(`Successfully created ${celebritiesFromDB.length} celebrities.`);
+
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(`Something went wrong with seeding the database: ${err}`));
---
+++ starter-code/routes/celebrities.routes.js
@@ -0,0 +1,8 @@
+// iteration #2 listing our celebrities
+const express = require("express");
+
+const celebrityRouter = express.Router();
+
+
+
+// localhost:3000/celebrities/new  => because we prefixed in app.js
